dfbd1db4d48cb33bd87069a87e36ec7f8c271c5f
Ivan Caldarella <gamein3x@gmail.com>
2026-04-14 16:54:42 +0200

    Snack 4 risolto

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authorsName = posts.map(name => name.author);
+console.log(authorsName);
