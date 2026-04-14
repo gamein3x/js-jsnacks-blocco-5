28ae3cad78eeefac52ed78211d5c00bd5782ee67
Ivan Caldarella <gamein3x@gmail.com>
2026-04-14 16:44:31 +0200

    Snack 1 risolto

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,6 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+console.log(names);
+names.forEach(name => console.log(name));
